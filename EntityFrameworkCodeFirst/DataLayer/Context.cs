da21e7912dc521a89c86e575e55978ce90c38f89
anowakowski <nowakowski.add@gmail.com>
2016-12-22 16:21:27 +0100

    Added new projcets: data layer and domain. Prepared first Context inheritance by dbContext

---
+++ EntityFrameworkCodeFirst/DataLayer/Context.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base()
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
---
+++ EntityFrameworkCodeFirst/Domain/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string BloggerName { get; set; }
+
+        public string Title { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
