ed61b417d6a1eddf31ae7f5263f2b87ab1d81ffa
anowakowski <nowakowski.add@gmail.com>
2016-12-23 14:18:37 +0100

    Added next part of data annotations.

--- EntityFrameworkCodeFirst/Domain/Alias_DataAnnotationsExample.cs
+++ EntityFrameworkCodeFirst/Domain/Alias_DataAnnotationsExample.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
+    [Table("AliasName")]
     public class Alias_DataAnnotationsExample
     {
-        public int Id { get; set; }
+        [Key]
+        public int AliasKey { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [MaxLength(20), MinLength(5)]
         public string UserName { get; set; }
+
+        [MinLength(5)]
         public string Email { get; set; }
+
         public string Bio { get; set; }
+
+        [Column("DateStarted", Order = 2)]
         public DateTime CreateDate { get; set; }
+
         public byte[] Avatar { get; set; }
+
         public ICollection<Tweet_DataAnnotationsExample> Tweets { get; set; }
     }
 }
